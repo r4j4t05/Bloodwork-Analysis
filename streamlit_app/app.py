17e327f517af3da9d9034c12dbf0ba43a152cb58
r4j4t05 <rajatsingh010305@gmail.com>
2026-08-29 22:30:41 +0530

    lastcommit

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -2,6 +2,9 @@ import streamlit as st
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
 
+st.title("RAG App Test")
+st.write("Streamlit UI loaded successfully")
+
 load_dotenv()
 
 llm= ChatGoogleGenerativeAI(model="gemini-3.6-flash")
